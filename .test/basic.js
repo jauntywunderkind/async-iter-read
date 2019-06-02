4189fdd9c807c68d750bbeb2eccdc03fc67d6ccf
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-06-02 16:48:55 -0400

    Add too many/too few tests.

--- .test/basic.js
+++ .test/basic.js
@@ -37,3 +37,63 @@ tape( "for-await", async function( t){
 
 	t.end()
 })
+
+tape( "ahead too many", async function( t){
+	const read= ahead( fixture(), 5)
+	t.ok( read instanceof Promise, "read is a promise")
+	t.equal( read.complete, 0, "nothing complete")
+
+	const r0= await read
+	t.equal( r0[ 0], 1, "1 value")
+	t.equal( r0[ 1], 2, "2 value")
+	t.equal( r0[ 2], 3, "3 value")
+	t.equal( r0[ 3], undefined, "no value")
+	t.equal( r0[ 4], undefined, "no value")
+	t.equal( read.complete, 5, "all complete")
+	t.equal( r0.length, 5, "length is ahead count")
+
+	t.end()
+})
+
+tape( "for-await too many", async function( t){
+	const read= forAwait( fixture(), 5)
+	t.ok( read instanceof Promise, "read is a promise")
+	t.equal( read.complete, 0, "nothing complete")
+
+	const r0= await read
+	t.equal( r0[ 0], 1, "1 value")
+	t.equal( r0[ 1], 2, "2 value")
+	t.equal( r0[ 2], 3, "3 value")
+	t.equal( read.complete, 3, "all complete")
+	t.equal( r0.length, 3, "length is read count")
+
+	t.end()
+})
+
+tape( "ahead too few", async function( t){
+	const read= ahead( fixture(), 2)
+	t.ok( read instanceof Promise, "read is a promise")
+	t.equal( read.complete, 0, "nothing complete")
+
+	const r0= await read
+	t.equal( r0[ 0], 1, "1 value")
+	t.equal( r0[ 1], 2, "2 value")
+	t.equal( read.complete, 2, "all complete")
+	t.equal( r0.length, 2, "length is n")
+
+	t.end()
+})
+
+tape( "for-await too few", async function( t){
+	const read= forAwait( fixture(), 2)
+	t.ok( read instanceof Promise, "read is a promise")
+	t.equal( read.complete, 0, "nothing complete")
+
+	const r0= await read
+	t.equal( r0[ 0], 1, "1 value")
+	t.equal( r0[ 1], 2, "2 value")
+	t.equal( read.complete, 2, "all complete")
+	t.equal( r0.length, 2, "length is n")
+
+	t.end()
+})
