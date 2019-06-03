51ac3868f11c4690b4f8ecd37aa4c11cbd3b7f83
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-06-03 15:48:14 -0400

    Add expect to async-iter-read.js.

--- async-iter-read.js
+++ async-iter-read.js
@@ -1,4 +1,5 @@
 "use module"
+export { expect, Expect} from "./expect.js"
 export { fixed, Fixed} from "./fixed.js"
 export { readForAwait, ReadForAwait} from "./for-await.js"
 export { rolling, Rolling} from "./rolling.js"
